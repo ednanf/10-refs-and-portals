d0cb6ed0b6b6755aca52f193069295d5b76d95d4
EDNAN FRIZZERA FILHO <ednan@frizzera.dev>
2024-03-07 09:03:35 -0300

    138

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+function ResultModal({ result, targetTime }) {
+	return (
+		<dialog className="result-modal">
+			<h2>You {result}</h2>
+			<p>
+				The target time was <strong>{targetTime}s</strong>
+			</p>
+			<p>
+				You stopped the timer with <strong>X seconds left.</strong>
+			</p>
+			<form method="dialog">
+				<button>Close</button>
+			</form>
+		</dialog>
+	);
+}
+
+export default ResultModal;
